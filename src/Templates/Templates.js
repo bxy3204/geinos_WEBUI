7797a6216df8b916b11ea172f17d0ae12810ebed
qxa7606 <aliqasim9907@gmail.com>
2018-08-04 13:49:24 -0400

    delete templates fix

--- src/Templates/Templates.js
+++ src/Templates/Templates.js
@@ -174,27 +174,8 @@ class Templates extends Component {
     }
 
     onDeleteRow(rowKeys) {
-        alert('You deleted: ' + rowKeys)
-        delete_templates(rowKeys).then((fetched) => {
-            fetched.json().then((data) => {
-                console.log(data)
-                this.setState({message: data.message})
-                /*
-                      Status codes between 400 and 500 are being forced to 400 because
-                      they map to a specific CSS style class labeled with that status code.
-                      The style name is used in render().
-                       */
-                if (data.status >= 400 && data.status < 500) {
-                    this.setState({status: 400})
-                } else if (data.status >= 200 && data.status < 300) {
-                    this.setState({status: 200})
-                } else {
-                    // any other status than success or error will be treated as 102, informational
-                    // this reflects in the css file to ensure proper message notification
-                    this.setState({status: 102})
-                }
-            })
-        })
+        alert('You deleted: ' + rowKeys);
+        delete_templates(rowKeys);
     }
 
     render() {
